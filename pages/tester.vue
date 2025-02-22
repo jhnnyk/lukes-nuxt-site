3c076175a27baf6ed74d0e3fa51c11c8a1efae96
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-02-21 23:02:16 -0700

    added a semicolon to test external js

--- pages/tester.vue
+++ pages/tester.vue
@@ -6,7 +6,7 @@
       <p>JavaScript can change HTML attribute values.</p>
       <p id="demo" style="color: red">Hm</p>
       <p id="demo2" style="color: red">Hm?</p>
-      <button onclick="testFunction(); testFunction2()">Test</button>
+      <button onclick="testFunction(); testFunction2();">Test</button>
       <Footer />
     </main>
   </div>
