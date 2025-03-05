0eec6749e7177b9da0cf26480d8816ab220390af
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-03-04 21:15:11 -0700

    working on new homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
-<template id="newlook">
-  <div id="newlook">
-    <Header />
-    <p>Sup, bro.</p>
+<script setup>
+useHead({
+  bodyAttrs: {
+    id: "newlook",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <p>
+      <a class="unlink" href="/about">Luke Knotts</a> |
+      <a class="unlink" href="/projects/projectshome">Game Design</a>
+    </p>
+    <a href="projects/thehunt">
+      <div class="thumbnail">
+        <img src="/images/thumbnails/thehunt.png" class="thumbnail-image" />
+        <p>Hello, here's some text.</p>
+      </div>
+    </a>
   </div>
 </template>
