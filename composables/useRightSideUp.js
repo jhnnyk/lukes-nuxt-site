34e9f0074913ea1a0e8a0c87104285e924e88922
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-03-24 17:21:58 -0600

    messing with composables and javascript and other misfortunes

---
+++ composables/useRightSideUp.js
@@ -0,0 +1,10 @@
+// code for Upside down button that you click when right side up.
+
+// This doesn't work when used in this separate .js file, the code only works in-line, I think because otherwise my website doesn't know where to find the document due to all my weird Nuxt stuff.
+
+export function useRightSideUp() {
+    // document.getElementById('whole').classList.add('upsidedown'); 
+    // document.getElementById('rightsideup').style.display='none'; 
+    // document.getElementById('upsidedown').style.display='inline'; 
+    // document.getElementById('upmessage').style.display='inline';
+}
