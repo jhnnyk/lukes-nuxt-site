e8e42cfc8e54d3de56ad2ad7284987e075b1fc45
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-03-24 21:31:56 -0600

    index page updates

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ useHead({
 <template>
   <div style="display: block">
     <NewHeader />
-
+    <br />
     <div class="flex-container thumbnails">
       <Thumbnail
         :image="`/images/thumbnails/thehunt.png`"
@@ -18,7 +18,7 @@ useHead({
         <div class="thumb-desc">
           <p class="underline">The Hunt (2025)</p>
           <p>
-            Team Size: <span class="dark-text">9</span> <br />
+            Team Size: <span class="dark-text">Nine</span> <br />
             Engine: <span class="dark-text">Unreal Engine 5</span> <br />
             Role: <span class="dark-text">Project Manager, Level Design</span>
             <br />
@@ -27,41 +27,28 @@ useHead({
         </div>
       </Thumbnail>
       <Thumbnail
-        :image="`/images/thumbnails/thehunt.png`"
-        :link="`/projects/thehunt`"
+        :image="`/images/thumbnails/occulinaryclub.png`"
+        :link="`/projects/occulinaryclub`"
       >
         <div class="thumb-desc">
-          <p class="underline">The Hunt (2025)</p>
+          <p class="underline">The Occulinary Club (2024)</p>
           <p>
-            Team Size: <span class="dark-text">9</span> <br />
-            Engine: <span class="dark-text">Unreal Engine 5</span> <br />
-            Role: <span class="dark-text">Project Manager, Level Design</span>
+            Team Size: <span class="dark-text">Six</span> <br />
+            Software:
+            <span class="dark-text">Netlify, Javascript, HTML, CSS</span> <br />
+            Role:
+            <span class="dark-text"
+              >Project Manager, Game Design, Web Development</span
+            >
             <br />
-            Made In: <span class="dark-text">&#8776; 48 hrs.</span>
-          </p>
-        </div>
-      </Thumbnail>
-      <Thumbnail
-        :image="`/images/thumbnails/thehunt.png`"
-        :link="`/projects/thehunt`"
-      >
-        <div class="thumb-desc">
-          <p class="underline">The Hunt (2025)</p>
-          <p>
-            Team Size: <span class="dark-text">9</span> <br />
-            Engine: <span class="dark-text">Unreal Engine 5</span> <br />
-            Role: <span class="dark-text">Project Manager, Level Design</span>
-            <br />
-            Made In: <span class="dark-text">&#8776; 48 hrs.</span>
+            Made In: <span class="dark-text">&#8776; 2 Weeks</span>
           </p>
         </div>
       </Thumbnail>
     </div>
+    <br />
+    <br />
 
-    <p>
-      Hello! tehwoigwfo whfhwoghwifghwdoghwd dif whoigwdifghwod fi
-      wgowdhfiwdhgiogwdiogfhiowdfhiodwgfodwfgowid dfw dwguofgwdoifgiodwfgho
-      dwgfhwdoigiowghiodwghf owdifgwdogwohgow
-    </p>
+    <p>Hello! Look at those cool published games! ;o;</p>
   </div>
 </template>
