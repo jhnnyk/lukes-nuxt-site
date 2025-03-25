4f4748612aefa7b7eebde9c8563f2503fa280845
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-03-24 21:11:48 -0600

    making index page look better

--- pages/index.vue
+++ pages/index.vue
@@ -10,24 +10,35 @@ useHead({
   <div style="display: block">
     <NewHeader />
 
-    <Thumbnail
-      :image="`/images/thumbnails/thehunt.png`"
-      :link="`/projects/thehunt`"
-    >
-      <p>The Hunt (2025)</p>
-    </Thumbnail>
-    <Thumbnail
-      :image="`/images/thumbnails/thehunt.png`"
-      :link="`/projects/thehunt`"
-    >
-      <p>The Hunt (2025)</p>
-    </Thumbnail>
-    <Thumbnail
-      :image="`/images/thumbnails/thehunt.png`"
-      :link="`/projects/thehunt`"
-    >
-      <p>The Hunt (2025)</p>
-    </Thumbnail>
+    <div class="flex-container thumbnails">
+      <Thumbnail
+        :image="`/images/thumbnails/thehunt.png`"
+        :link="`/projects/thehunt`"
+      >
+        <div class="thumb-desc">
+          <p class="underline">The Hunt (2025)</p>
+          <p>
+            Team Size: <span class="dark-text">9</span> <br />
+            Engine: <span class="dark-text">Unreal Engine 5</span> <br />
+            Role: <span class="dark-text">Project Manager, Level Design</span>
+            <br />
+            Made In: <span class="dark-text">&#8776; 48 hrs.</span>
+          </p>
+        </div>
+      </Thumbnail>
+      <Thumbnail
+        :image="`/images/thumbnails/thehunt.png`"
+        :link="`/projects/thehunt`"
+      >
+        <p>The Hunt (2025)</p>
+      </Thumbnail>
+      <Thumbnail
+        :image="`/images/thumbnails/thehunt.png`"
+        :link="`/projects/thehunt`"
+      >
+        <p>The Hunt (2025)</p>
+      </Thumbnail>
+    </div>
 
     <p>
       Hello! tehwoigwfo whfhwoghwifghwdoghwd dif whoigwdifghwod fi
