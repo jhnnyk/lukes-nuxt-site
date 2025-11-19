9ec30a24d48ad219f0b419dff7acc202e810ae24
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-11-19 10:02:00 -0700

    made page for copy pasting characters

---
+++ pages/copypaste.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <h1>Copy Paste Symbols</h1>
+    <hr />
+    <h2>Single Characters:</h2>
+    <tr>
+      <th>Pi</th>
+      <th>&#x1d70b;</th>
+    </tr>
+    <tr>
+      <th>Euler's Number "e"</th>
+      <th>&#x1d452;</th>
+    </tr>
+    <tr>
+      <th>Theta</th>
+      <th>&#x03b8;</th>
+      <th>Theta (italic)</th>
+      <th>&#x1d703;</th>
+    </tr>
+    <tr>
+      <th>Phi</th>
+      <th>&#x03c6;</th>
+      <th>Phi (italic)</th>
+      <th>&#x1d711;</th>
+    </tr>
+    <tr>
+      <th>Infinity</th>
+      <th>&infin;</th>
+    </tr>
+    <tr>
+      <th>Union</th>
+      <th>&#x22c3;</th>
+    </tr>
+    <tr>
+      <th>Empty Set</th>
+      <th>&#x2205;</th>
+    </tr>
+    <tr>
+      <th>Summation</th>
+      <th>&#x2211;</th>
+    </tr>
+    <tr>
+      <th>Constantine</th>
+      <th>&#x2102;</th>
+      <th>&#x2102;onstantine</th>
+    </tr>
+    <tr>
+      <th>Cent</th>
+      <th>&cent;</th>
+    </tr>
+    <tr>
+      <th>Italic Pi</th>
+      <th>&#x1d71b;</th>
+    </tr>
+    <h2>Phrases:</h2>
+    <tr>
+      <th>Beautiful equation</th>
+      <th>&lceil;&#x1d452;&rceil; = &lfloor;&#x1d70b;&rfloor;</th>
+    </tr>
+    <tr>
+      <th>Luke</th>
+      <th>&#x30eb;&#x30fc;&#x30af;</th>
+    </tr>
+  </div>
+</template>
+
+<style>
+/* Generic Styles */
+
+body {
+  background-color: white;
+
+  padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
+
+  width: 90%;
+  max-width: 1000px;
+}
+
+div {
+  margin: none;
+  padding: none;
+
+  width: 100%;
+}
+
+h1 {
+  color: black;
+
+  font-family: "Times New Roman", Times, serif;
+
+  font-size: 28px;
+}
+
+h2 {
+  text-decoration: underline;
+
+  font-size: 22px;
+
+  color: rgb(107, 107, 164);
+}
+
+p,
+tr,
+th {
+  color: black;
+
+  font-family: "Times New Roman", Times, serif;
+
+  font-size: 20px;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  padding: 5px;
+  padding-right: 40px;
+
+  border-right: 2px;
+  border-color: black;
+}
+</style>
