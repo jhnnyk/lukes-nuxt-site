95b011e8369ee0e765c245dd1cf487e0f321eedd
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-11-19 19:24:09 -0700

    new flash card page

---
+++ components/content/FlashCard.vue
@@ -0,0 +1,65 @@
+<script setup>
+defineProps(["term", "def"]);
+
+var flipcardfront = ref("showface");
+var flipcardback = ref("hideface");
+
+function flip() {
+  if (flipcardfront.value == "showface") {
+    flipcardfront.value = "hideface";
+    flipcardback.value = "showface";
+  } else {
+    flipcardfront.value = "showface";
+    flipcardback.value = "hideface";
+  }
+}
+</script>
+
+<template>
+  <div class="flip-card">
+    <div class="flip-card-outer">
+      <div class="flip-card-inner" @click="flip">
+        <div :class="flipcardfront">
+          <div class="front">
+            <p>{{ term }}</p>
+          </div>
+        </div>
+        <div :class="flipcardback">
+          <div class="back">
+            <p>{{ def }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.showface {
+  display: visible;
+  transition: 0.2s;
+}
+
+.hideface {
+  display: none;
+  transition: 0.2s;
+}
+
+.flip-card-inner {
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px;
+
+  margin: 5px;
+}
+.front {
+  background-color: rgb(178, 178, 178);
+  height: 200px;
+  width: 500px;
+}
+.back {
+  background-color: rgb(148, 175, 198);
+  height: 200px;
+  width: 500px;
+}
+</style>
---
+++ composables/cards.js
@@ -0,0 +1,148 @@
+export let mycards = [
+
+    // --------------------------------------------------------------
+    // Visualize Quadrants
+    // --------------------------------------------------------------
+
+    {
+        // 'top right' Quadrant
+        id: 1,
+        term: "Which quadrant is in the 'top right'?",
+        def: "Quadrant I"
+    },
+    {
+        // 'top left' Quadrant
+        id: 2,
+        term: "Which quadrant is in the 'top left'?",
+        def: "Quadrant II"
+    },
+    {
+        // 'bottom left' Quadrant
+        id: 3,
+        term: "Which quadrant is in the 'bottom left'?",
+        def: "Quadrant III",
+    },
+    {
+        // 'bottom right' Quadrant
+        id: 4,
+        term: "Which quadrant is in the 'bottom right'?",
+        def: "Quadrant IV",
+    },
+
+    // --------------------------------------------------------------
+    // convert Degrees to Radians
+    // --------------------------------------------------------------
+
+    {
+        // 90 deg --> radians
+        id: 5,
+        term: "What is \\( 90 ^{\\circ} \\) in radians?",
+        def: "\\( 90 ^{\\circ} \\) is \\( \\frac{\\pi}{2} \\) radians."
+        // 60 deg is pi/2 radians
+    },
+    {
+        // 45 deg --> radians
+        id: 6,
+        term: "What is \\( 45 ^{\\circ} \\) in radians?",
+        def: "\\( 45 ^{\\circ} \\) is \\( \\frac{\\pi}{4} \\) radians."
+        // 45 degrees is pi/4 radians
+    },
+    {
+        // 60 deg --> radians
+        id: 7,
+        term: "What is \\( 60 ^{\\circ} \\) in radians?",
+        def: "\\( 60 ^{\\circ} \\) is \\( \\frac{\\pi}{3} \\) radians."
+        // 60 degrees is pi/3 radians
+    },
+    {
+        // 360 deg --> radians
+        id: 8,
+        term: "What is \\( 360 ^{\\circ} \\) in radians?",
+        def: "\\( 360 ^{\\circ} \\) is \\( 2 \\pi \\) radians."
+        // 360 degrees is 2pi radians
+    },
+    {
+        // 30 deg --> radians
+        id: 9,
+        term: "What is \\( 30 ^{\\circ} \\) in radians?",
+        def: "\\( 30 ^{\\circ} \\) is \\( \\frac{\\pi}{6} \\) radians."
+        // 30 degrees is pi/6 radians
+    },
+    {
+        // 180 deg --> radians
+        id: 10,
+        term: "What is \\( 180 ^{\\circ} \\) in radians?",
+        def: "\\( 180 ^{\\circ} \\) is \\( \\pi \\) radians."
+        // 180 degrees is pi radians.
+    },
+    {
+        // 120 deg --> radians
+        id: 11,
+        term: "What is \\( 120 ^{\\circ} \\) in radians?",
+        def: "\\( 120 ^{\\circ} \\) is \\( \\frac{2\\pi}{3} \\) radians."
+        // 120 degrees is 2pi/3 radians.
+    },
+    {
+        // 150 deg --> radians
+        id: 12,
+        term: "What is \\( 150 ^{\\circ} \\) in radians?",
+        def: "\\( 150 ^{\\circ} \\) is \\( \\frac{5\\pi}{6} \\) radians."
+        // 150 degrees is 5pi/6 radians.
+    },
+    {
+        // 135 deg --> radians
+        id: 13,
+        term: "What is \\( 135 ^{\\circ} \\) in radians?",
+        def: "\\( 135 ^{\\circ} \\) is \\( \\frac{3\\pi}{4} \\) radians."
+        // 135 degrees is 3pi/4 radians.
+    },
+    {
+        // 210 def --> radians
+        id: 14,
+        term: "What is \\( 210 ^{\\circ} \\) in radians?",
+        def: "\\( 210 ^{\\circ} \\) is \\( \\frac{7\\pi}{6} \\) radians."
+        // 210 degrees is 7pi/6 radians.
+    },
+    {
+        // 225 deg --> radians
+        id: 15,
+        term: "What is \\( 225 ^{\\circ} \\) in radians?",
+        def: "\\( 225 ^{\\circ} \\) is \\( \\frac{5\\pi}{4} \\) radians."
+        // 225 degrees is 5pi/4 radians.
+    },
+    {
+        // 240 deg --> radians
+        id: 16,
+        term: "What is \\( 240 ^{\\circ} \\) in radians?",
+        def: "\\( 240 ^{\\circ} \\) is \\( \\frac{4\\pi}{3} \\) radians."
+        // 240 degrees is 4pi/3 radians.
+    },
+    {
+        // 270 deg --> radians
+        id: 17,
+        term: "What is \\( 270 ^{\\circ} \\) in radians?",
+        def: "\\( 270 ^{\\circ} \\) is \\( \\frac{3\\pi}{2} \\) radians."
+        // 270 degrees is 3pi/2 radians.
+    },
+    {
+        // 300 deg --> radians
+        id: 18,
+        term: "What is \\( 300 ^{\\circ} \\) in radians?",
+        def: "\\( 300 ^{\\circ} \\) is \\( \\frac{5\\pi}{3} \\) radians."
+        // 300 degrees is 5pi/3 radians.
+    },
+    {
+        // 315 deg --> radians
+        id: 19,
+        term: "What is \\( 315 ^{\\circ} \\) in radians?",
+        def: "\\( 315 ^{\\circ} \\) is \\( \\frac{7\\pi}{4} \\) radians.",
+        // 315 degrees is 7pi/4 radians.
+    },
+    {
+        // 330 deg --> radians
+        id: 20,
+        term: "What is \\( 330 ^{\\circ} \\) in radians?",
+        def: "\\( 330 ^{\\circ} \\) is \\( \\frac{11\\pi}{6} \\) radians."
+        // 330 degrees is 11pi/6 radians.
+    }
+];
