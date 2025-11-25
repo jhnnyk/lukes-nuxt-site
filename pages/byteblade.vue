71b2e9bac9ae4c8c0ff4ef416ff557bc2e27380d
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-11-25 01:28:42 -0600

    updates to byteblade.vue

--- pages/byteblade.vue
+++ pages/byteblade.vue
@@ -6,29 +6,72 @@ var started = false;
 
 var player_location = [0, 0];
 
-const c_move_forward = ["forward", "go", "move", "walk"];
-const c_fight = ["fight", "die", "kill"];
+// general commands
+const c_quit = [
+  "quit",
+  "giveup",
+  "end",
+  "gameover",
+  "q",
+  "clear",
+  "reload",
+  "reset",
+];
+function f_quit() {
+  location.reload();
+}
+// any unknown commands
+function f_unknown() {
+  content.value = "Unknown command. Type 'HELP' for more info.";
+}
+const c_help = ["help", "h", "helpme", "idk"];
+function f_help() {
+  content.value =
+    "This is a game. Type and press enter. Use QUIT to end or restart.";
+}
 
+// player actions
+const c_move_forward = ["forward", "go", "move", "walk"];
 function f_move_forward() {
   content.value = "hi!!!";
 }
+const c_fight = ["fight", "die", "kill"];
 function f_fight() {
-  content.value = "yay";
+  content.value = "You approach the door. You sit before the door.";
 }
 
+// when the player presses enter
 function submit() {
   message.value = message.value.toLowerCase();
-  message.value = message.value.replace(/[\s.!?]/g, "");
+  message.value = message.value.replace(/[\s.!?,'"-_]/g, "");
 
+  // general functions
+  if (c_quit.includes(message.value)) {
+    f_quit();
+  }
+  if (c_help.includes(message.value)) {
+    f_help();
+    message.value = "";
+    return;
+  }
+
+  // check if first time player interacting
   if (!started) {
     content.value = "There is a door before you.";
     started = true;
   } else {
+    // player actions
     switch (true) {
       case c_move_forward.includes(message.value):
         f_move_forward();
+        break;
       case c_fight.includes(message.value):
         f_fight();
+        break;
+
+      // unknown command
+      default:
+        f_unknown();
     }
   }
   message.value = "";
@@ -68,6 +111,7 @@ p {
 
 .content {
   height: 60px;
+  width: 300px;
 }
 
 h1 {
