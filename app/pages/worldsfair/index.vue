02e74d35c3768194c63f5a6a1a2ce72467477833
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-02-15 01:02:59 -0700

    messing with worldsfair page

--- app/pages/worldsfair/index.vue
+++ app/pages/worldsfair/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-      <h1>World's Fair!</h1>
-      <p>How is it going?</p>
+    <h1>World's Fair!</h1>
+    <p>How is it going?</p>
   </div>
 </template>
+
+<style scoped>
+h1 {
+    font-family: arial;
+}
+
+
+</style>
